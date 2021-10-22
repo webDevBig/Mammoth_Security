4d591457049ea5a7ceec38371b410afd9e2b6c96
Olena Kushnir <olenadev7@gmail.com>
2021-10-22 14:41:03 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -1,10 +1,31 @@
-//var open_menu = document.querySelectorAll('.mobile_btn');
-//[].forEach.call(open_menu, function (el) {
-//    el.onclick = function (e) {
+var open_sub_menu = document.querySelectorAll('.open_sub_menu');
+[].forEach.call(open_sub_menu, function (el) {
+    el.onclick = function (e) {
+        el.parentElement.querySelector('.mobile_sub_menu_box').classList.toggle('mobile_sub_menu_box_open')
+        el.querySelector('.mobile_arrow').classList.toggle('arrow_transform')
+        el.classList.toggle('active') 
+    }
+});
+
+var open_sub_sub_menu = document.querySelectorAll('.open_sub_sub_menu');
+[].forEach.call(open_sub_sub_menu, function (el) {
+    el.onclick = function (e) {
+        el.parentElement.querySelector('.sub_menu').classList.toggle('sub_menu_open')
+        el.querySelector('.mobile_arrow').classList.toggle('arrow_transform')
+        
+        el.classList.toggle('active') 
+    }
+});
+
+var open_menu = document.querySelectorAll('.mobile_btn');
+[].forEach.call(open_menu, function (el) {
+    el.onclick = function (e) {
 //        document.querySelector(".main_menu").classList.toggle('mobile_menu_open');
-//        document.querySelector("body").classList.toggle('body')
-//    }
-//});
+        
+         document.querySelector(".line_nav_bottom").classList.toggle('open');
+        document.querySelector("body").classList.toggle('body')
+    }
+});
 //var close_menu = document.querySelectorAll('.close_menu');
 //[].forEach.call(close_menu, function (el) {
 //    el.onclick = function (e) {
